43e10aa74d2c2e06872120e1056a4de085c9ebe5
ZsJuhaszZalan <zsiros-juhasz.zalan@diak.szbi-pg.hu>
2025-01-13 22:28:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/tabla')
+def tabla():
 plants = [
     {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
     {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
     {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
 ]
-
+return render_template('tabla.html', plants=plants)
+if __name__ == '__main__':
+    app.run(debug=True)
