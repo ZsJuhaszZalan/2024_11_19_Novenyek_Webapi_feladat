7b9674f6da632730ff08b4ca4e77d8efbe4e94d8
ZsJuhaszZalan <zsiros-juhasz.zalan@diak.szbi-pg.hu>
2025-01-14 09:37:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def index():
 
 @app.route('/tabla')
 def tabla():
-plants = [
-    {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
-    {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
-    {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
-]
-return render_template('tabla.html', plants=plants)
+    plants = [
+        {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
+        {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
+        {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
+    ]
+    return render_template('tabla.html', plants=plants)
 if __name__ == '__main__':
     app.run(debug=True)
